21c4614370925e5b3a4d8c2079e8684a292dd0e1
Jerin <jerinji2016@gmail.com>
2024-11-03 19:40:03 +0400

    implement service and models

---
+++ src/pkg/service/posts_service.go
@@ -0,0 +1,54 @@
+package service
+
+import (
+	"context"
+
+	"github.com/Jerinji2016/grpc-template/src/internal/models"
+	"github.com/Jerinji2016/grpc-template/src/pkg/pb"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type PostService struct {
+	pb.UnimplementedPostServiceServer
+	posts map[string]*models.Post
+}
+
+func NewPostServce() *PostService {
+	return &PostService{
+		posts: make(map[string]*models.Post),
+	}
+}
+
+func (s *PostService) CreatePost(ctx context.Context, req *pb.CreatePostRequest) (*pb.CreatePostResponse, error) {
+	post := &models.Post{
+		ID:        req.Post.Id,
+		Message:   req.Post.Message,
+		CreatedAt: req.Post.CreatedAt,
+	}
+
+	s.posts[post.ID] = post
+
+	return &pb.CreatePostResponse{
+		Post: &pb.Post{
+			Id:        post.ID,
+			Message:   post.Message,
+			CreatedAt: post.CreatedAt,
+		},
+	}, nil
+}
+
+func (s *PostService) GetPost(ctx context.Context, req *pb.GetPostRequest) (*pb.GetPostResponse, error) {
+	post, exists := s.posts[req.Id]
+    if !exists {
+        return nil, status.Error(codes.NotFound, "resource not found")
+    }
+
+    return &pb.GetPostResponse{
+        Post: &pb.Post{
+            Id:        post.ID,
+            Message:   post.Message,
+            CreatedAt: post.CreatedAt,
+        },
+    }, nil
+}
